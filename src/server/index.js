69d52cd50b694049a29ed5272c4a247b6e7bff98
Yahor Simak <Yahor_Simak@epam.com>
2017-12-26 18:03:04 +0300

    add basic react ssr template (wip)

---
+++ src/server/index.js
@@ -0,0 +1,66 @@
+import express from "express";
+import cors from "cors";
+import React from "react";
+import { renderToString } from "react-dom/server";
+import { Provider } from "react-redux";
+import { StaticRouter, matchPath } from "react-router-dom";
+import serialize from "serialize-javascript";
+import routes from "../shared/routes";
+import configureStore from "../shared/configureStore";
+import App from "../shared/App";
+import "source-map-support/register";
+
+const app = express();
+let dataUrl = `https://se-ssr-react.firebaseio.com/data`;
+
+app.use(cors());
+app.use(express.static("public"));
+
+app.get(dataUrl, (req, res) => {
+  res.json();
+});
+
+app.get("*", (req, res, next) => {
+  const store = configureStore();
+
+  const promises = routes.reduce((acc, route) => {
+    if (matchPath(req.url, route) && route.component && route.component.initialAction) {
+      acc.push(Promise.resolve(store.dispatch(route.component.initialAction())));
+    }
+    return acc;
+  }, []);
+
+  Promise.all(promises)
+    .then(() => {
+      const context = {};
+      const markup = renderToString(
+        <Provider store={store}>
+          <StaticRouter location={req.url} context={context}>
+            <App />
+          </StaticRouter>
+        </Provider>
+      );
+
+      const initialData = store.getState();
+      res.send(`
+        <!DOCTYPE html>
+        <html>
+          <head>
+            <title>W Combinator</title>
+            <link rel="stylesheet" href="/css/main.css">
+            <script src="/bundle.js" defer></script>
+            <script>window.__initialData__ = ${serialize(initialData)}</script>
+          </head>
+
+          <body>
+            <div id="root">${markup}</div>
+          </body>
+        </html>
+      `);
+    })
+    .catch(next);
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server is listening");
+});
